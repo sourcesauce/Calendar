649e150adbf42bbb7590c424b038771da7990687
SO <SO@DESKTOP-GDR4G99>
2018-11-02 02:04:27 +0900

    Prompt 추가

--- src/so/calendar/Calendar.java
+++ src/so/calendar/Calendar.java
@@ -10,33 +10,23 @@ public class Calendar {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleCalendar() {
-		System.out.println("일  월  화  수  목  금  토");
-		System.out.println("-----------------");
-		System.out.println("1  2  3  4  5  6  7");
-		System.out.println("8  9  10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		System.out.println("29 30 31");
-
-	}
-
-	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		System.out.println("반복 횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
-
-		for (int i = 0; i < repeat; i++) {
-			System.out.println("달을 입력하세요.");
-			int month = scanner.nextInt();
-
-			System.out.println(month + "월 은 " + cal.getMaxDaysOfMonth(month) + "일 까지 있습니다.");
+	public void printCalendar(int year,int month) {
+		System.out.printf("   <<%4d년%3d월>>\n",year,month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("--------------------");
+		
+		int maxDay= getMaxDaysOfMonth(month);
+		for(int i=1; i<=maxDay;i++) {
+			System.out.printf("%3d",i);
+			if(i%7==0)
+				System.out.println();
 		}
-		System.out.println("BYE");
-		scanner.close();
-		// cal.printSampleCalendar();
-	}
+		System.out.println();
+//		System.out.println("1  2  3  4  5  6  7");
+//		System.out.println("8  9  10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+//		System.out.println("29 30 31");
 
+	}
 }
---
+++ src/so/calendar/Prompt.java
@@ -0,0 +1,36 @@
+package so.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal> ";
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month,year = 0;
+		while (true) {
+			System.out.println("달을 입력하세요.(-1입력시 종료)");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
+			cal.printCalendar(2017,month);
+		}
+		System.out.println("BYE");
+		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		// 셀 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}
+}
