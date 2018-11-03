810f63ee5a915f2cde986f0f0000e725d319576b
SO <SO@DESKTOP-GDR4G99>
2018-11-04 07:55:32 +0900

    Calendar 추가

--- src/so/calendar/Calendar.java
+++ src/so/calendar/Calendar.java
@@ -1,7 +1,5 @@
 package so.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -12,34 +10,44 @@ public class Calendar {
 			return true;
 		else
 			return false;
-
 	}
-	public int getMaxDaysOfMonth(int year,int month){
-		if(isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month-1];
-		}else {
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
 			return MAX_DAYS[month - 1];
 		}
 	}
-	
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.println("--------------------");
+		System.out.println("----------------------");
 
-		int maxDay = getMaxDaysOfMonth(year,month);
-		for (int i = 1; i <= maxDay; i++) {
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = count < 7 ? count : 0;
+		// print first line
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0)
+		}
+		System.out.println();
+
+		// print from second line to last
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim)
 				System.out.println();
 		}
 		System.out.println();
-//		System.out.println("1  2  3  4  5  6  7");
-//		System.out.println("8  9  10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
-//		System.out.println("29 30 31");
+		System.out.println();
 
 	}
 }
--- src/so/calendar/Prompt.java
+++ src/so/calendar/Prompt.java
@@ -4,27 +4,60 @@ import java.util.Scanner;
 
 public class Prompt {
 
+	/*
+	 * @param week 요일명
+	 * 
+	 * @return 0~6(0=Sunday,6=Saturday)
+	 */
+	public int parseDay(String week) {
+		if (week.equals("su"))
+			return 0;
+		else if (week.equals("mo"))
+			return 1;
+		else if (week.equals("tu"))
+			return 2;
+		else if (week.equals("we"))
+			return 3;
+		else if (week.equals("th"))
+			return 4;
+		else if (week.equals("fr"))
+			return 5;
+		else if (week.equals("sa"))
+			return 6;
+		else
+			return 0;
+	}
+
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int month,year = 0;
+		int month, year, weekday = 0;
+
 		while (true) {
-			
+
 			System.out.println("연도를 입력하세요.");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
+			if(year==-1) {
+				break;
+			}
 			
 			System.out.println("달을 입력하세요.(-1입력시 종료)");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			if (month == -1) {
-				break;
-			}
-			if (month > 12) {
+			if (month > 12 || month<1) {
+				System.err.println("잘못된 입력입니다.");
 				continue;
 			}
-			cal.printCalendar(year,month);
+			
+			System.out.println("첫째날의 요일을 입력하세요(su,mo,tu,we,th,fr,sa)");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+
+			
+			
+			cal.printCalendar(year, month, weekday);
 		}
 		System.out.println("BYE");
 		scanner.close();
